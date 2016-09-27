c16530d7037b1e07535925a730352794cc64fbf9
Scott Rafael <w0218584@nscc.ca>
2016-09-27 00:07:57 -0300

    Step 1b

---
+++ Lab1/Lab1Step1B.js
@@ -0,0 +1,24 @@
+"use strict";
+
+function navigateHeadings(selectedElement)
+{
+    var headingNodes = document.getElementsByClassName("accordHeader");
+    var infoNodes = document.getElementsByClassName("info");
+    var selectedInfo = selectedElement.parentNode.childNodes[3];
+
+    if (selectedInfo.style.display === "none"){
+        var selectState = true;
+    }
+
+    for (var i = 0; i < headingNodes.length; i++){
+        headingNodes[i].style.backgroundColor = "blueviolet";
+        headingNodes[i].style.color = "black";
+        infoNodes[i].childNodes[3].style.display = "none";
+    }
+
+    if(selectState){
+        selectedElement.style.backgroundColor = "indigo";
+        selectedElement.style.color = "white";
+        selectedInfo.style.display = "block";
+    }
+}
