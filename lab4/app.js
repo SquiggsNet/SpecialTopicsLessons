ae310f62f3eea848d761fd437349e3063a6bd460
Scott Rafael <w0218584@nscc.ca>
2016-11-08 22:59:23 -0400

    fixed student population and display added validation against already entered number

--- lab4/app.js
+++ lab4/app.js
@@ -2,13 +2,35 @@
 
     var app = angular.module('labApp', []);
     app.controller('labController', function ($scope) {
-        //define array of todo items
+
         $scope.students = [];
 
+        function Student(number, firstName, lastName)
+        {
+            this.number = number;
+            this.firstName = firstName;
+            this.lastName = lastName;
+
+            this.printStudentDetails = function(){
+                return this.number+" - "+this.firstName+" "+this.lastName;
+            }
+        }
+
         $scope.addStudent = function () {
-            if($scope.sNumber.length || $scope.sNumber.fname || $scope.lname.length){
-                // studentObj = new Student({number: $scope.sNumber},{fname: $scope.fname},{lname: $scope.lname});
-                $scope.students.push({snumber: $scope.sNumber,fname: $scope.fName,lname: $scope.lName});
+            if($scope.sNumber.length && $scope.fName.length && $scope.lName.length){
+
+                studentObj = new Student($scope.sNumber, $scope.fName, $scope.lName);
+
+                if($scope.students.length){
+                    for(i=0;i<$scope.students.length;i++){
+                        if(studentObj.number==$scope.students[i].number){
+                            alert("this student number is already in use");
+                            return;
+                        }
+                    }
+                }
+
+                $scope.students.push(studentObj);
                 $scope.sNumber = "";
                 $scope.fName = "";
                 $scope.lName = "";
